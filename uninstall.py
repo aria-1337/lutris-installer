593efd3111db42bbff7be511fef82a25733736e9
aria-1337 <aria.lopez.dev@gmail.com>
2024-02-26 18:08:45 -0800

    Actually works now, cleans all the files

--- uninstall.py
+++ uninstall.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import shutil
 from os import listdir
 from os.path import isfile, join
 
@@ -11,3 +12,9 @@ files = [f for f in listdir('./') if isfile(join('./', f))]
 for f in files:
     if "lutris" in f:
         os.remove(f)
+
+# cleanup all other files
+home_path = os.path.expanduser('~')
+shutil.rmtree(os.path.join(home_path, '.cache/lutris'))
+shutil.rmtree(os.path.join(home_path, '.local/share/lutris'))
+shutil.rmtree(os.path.join(home_path, 'Games'))
